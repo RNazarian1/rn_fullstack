103d19782fba75d299a14028a0de2c5eee21cf83
RNazarian1 <nazarianreza@gmail.com>
2021-11-22 21:49:37 +0000

    genre set not mandatory in  the model.py

--- products/models.py
+++ products/models.py
@@ -21,7 +21,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     title = models.CharField(max_length=254)
     authur = models.CharField(max_length=254)
-    genre = models.CharField(max_length=254)
+    genre = models.CharField(max_length=64, null=True, blank=True)
     company = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
