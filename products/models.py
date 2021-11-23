616abf1ef3e5ec357404e2826fef7ff38ede9a47
RNazarian1 <nazarianreza@gmail.com>
2021-11-23 00:56:39 +0000

    admin url page is customised from admin.py and model.py verbose

--- products/models.py
+++ products/models.py
@@ -4,8 +4,10 @@ from django.db import models
 
 from django.db import models
 
-
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -25,9 +27,11 @@ class Product(models.Model):
     company = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.title
+
