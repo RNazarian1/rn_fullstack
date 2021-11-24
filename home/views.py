248dafd831698e3008465f933da06f316ddb7743
RNazarian1 <nazarianreza@gmail.com>
2021-11-24 00:18:50 +0000

    and error to need to fixed before further progress

--- home/views.py
+++ home/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 # Create your views here.
 
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
 
-def index(request):
-    """A view to return index page"""
+    products = Product.objects.all()
 
-    return render(request, 'home/index.html')
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+
+def products_detail(request, product_id):
+    """ A view to show indivisual product details """
+
+    products = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
