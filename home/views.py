9aef3147ee01aed047080f4aacfa26c31c3f91e5
RNazarian1 <nazarianreza@gmail.com>
2021-11-24 03:43:31 +0000

    basic search functionality is added

--- home/views.py
+++ home/views.py
@@ -1,26 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render
 
 # Create your views here.
 
-def all_products(request):
-    """ A view to show all products, including sorting and search queries """
 
-    products = Product.objects.all()
+def index(request):
+    """A view to return index page"""
 
-    context = {
-        'products': products,
-    }
-
-    return render(request, 'products/products.html', context)
-
-def products_detail(request, product_id):
-    """ A view to show indivisual product details """
-
-    products = get_object_or_404(Product, pk=product_id)
-
-    context = {
-        'product': product,
-    }
-
-    return render(request, 'products/product_detail.html', context)
+    return render(request, 'home/index.html')
