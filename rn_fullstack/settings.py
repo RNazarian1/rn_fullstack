e83decd5b644ca7b58b39f942032968afa2e1bae
RNazarian1 <nazarianreza@gmail.com>
2021-11-29 21:33:43 +0000

    secret key is defined

--- rn_fullstack/settings.py
+++ rn_fullstack/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h6$1j=f_q#yhl6hq2&h^#13kjv+vhd)_3q-p_o469(*8$e7=$7'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
